8f09cc91f20b3281d04a904b9053168d8c649cd5
Jens Alfke <jens@mooseyard.com>
2023-01-12 10:04:42 -0800

    Initial Commit

---
+++ DataWorld/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  DataWorld
+//
+//  Created by Jens Alfke on 1/12/23.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
